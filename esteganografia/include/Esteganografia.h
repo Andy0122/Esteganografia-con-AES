e32e8d480cda3b46e05f5c04f0edbef08c971c91
a-alfa19 <a.alfaro@estudiantec.cr>
2024-11-08 22:14:18 -0600

    Modulo de Esteganografia configurado

---
+++ esteganografia/include/Esteganografia.h
@@ -0,0 +1,16 @@
+//
+// Created by andres on 11/8/24.
+//
+
+#ifndef ESTEGANOGRAFIA_H
+#define ESTEGANOGRAFIA_H
+
+namespace ESTEGANOGRAFIA {
+
+class Esteganografia {
+
+};
+
+} // ESTEGANOGRAFIA
+
+#endif //ESTEGANOGRAFIA_H
---
+++ esteganografia/src/Esteganografia.cpp
@@ -0,0 +1,8 @@
+//
+// Created by andres on 11/8/24.
+//
+
+#include "Esteganografia.h"
+
+namespace ESTEGANOGRAFIA {
+} // ESTEGANOGRAFIA
